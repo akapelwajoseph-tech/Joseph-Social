c93f217f93af62529c5b439439362ad22cdfb439
akapelwajoseph-tech <akapelwajoseph@icloud.com>
2026-08-31 09:00:22 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,2 +1,7 @@
-const SUPABASE_URL = "https://totlrupzpuavrrdlxetm.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://totlrupzpuavrrdlxetm.supabase.co";
 const SUPABASE_KEY = "sb_publishable_D6wxRHeQF0FOZ-3tBxivuw_HvwRKGxV";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
